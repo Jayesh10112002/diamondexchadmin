a0ed93862da166b94dd07f992150ca4c29dbf595
Ankit chopra <chopraankit1608@gmail.com>
2025-02-06 15:08:58 +0530

    dode

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className='header-parent'>Diamond Admin Panel</div>
+  )
+}
+
+export default Header
