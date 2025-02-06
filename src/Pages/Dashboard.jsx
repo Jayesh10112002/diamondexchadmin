c05a5bac272f40a408aa588ac21cc7ece377dd35
Ankit chopra <chopraankit1608@gmail.com>
2025-02-06 15:14:49 +0530

    code

---
+++ src/Pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../Style/Dashboard.css"
+
+const Dashboard = () => {
+  return (
+    <div className='Dashboard-parent'> 
+    Dashboard of Admin
+        
+    </div>
+  )
+}
+
+export default Dashboard
