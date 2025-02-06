d933f12dbebf4bec8efbdcb4533531887e440048
Ankit chopra <chopraankit1608@gmail.com>
2025-02-06 17:07:47 +0530

    code

--- src/Components/DateFilter/DateFilter.jsx
+++ src/Components/DateFilter/DateFilter.jsx
@@ -1,9 +1,34 @@
-import React from "react";
-import { DatePicker, theme, Select } from "antd";
+import React, { useReducer } from "react";
+import { DatePicker, Select, theme } from "antd";
 import "./DateFilter.css";
+
+const initialState = {
+  dateRange: [],
+  select1: "lucy",
+  select2: "lucy",
+  searchText: "",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_DATE_RANGE":
+      return { ...state, dateRange: action.payload };
+    case "SET_SELECT1":
+      return { ...state, select1: action.payload };
+    case "SET_SELECT2":
+      return { ...state, select2: action.payload };
+    case "SET_SEARCH_TEXT":
+      return { ...state, searchText: action.payload };
+    case "RESET":
+      return initialState;
+    default:
+      return state;
+  }
+};
 
 const DateFilter = () => {
   const { token } = theme.useToken();
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const style = {
     backgroundColor: token.colorPrimary,
@@ -25,68 +50,84 @@ const DateFilter = () => {
       </div>
     );
   };
-  const handleChange = (value) => {
-    console.log(`selected ${value}`);
+
+  const handleSubmit = () => {
+    console.log("Submitted Values:", state);
   };
 
   return (
     <div className="DateFilter-parent">
       <div className="datefilter-child">
-        <DatePicker.RangePicker
-          cellRender={cellRender}
-          className="rangePicker"
-        />
-        <Select
-          prefix="User"
-          defaultValue="lucy"
-          onChange={handleChange}
-          className="select-part-report"
-          options={[
-            {
-              value: "jack",
-              label: "Jack",
-            },
-            {
-              value: "lucy",
-              label: "Lucy",
-            },
-            {
-              value: "Yiminghe",
-              label: "yiminghe",
-            },
-          ]}
-        />
-        <Select
-          prefix="User"
-          defaultValue="lucy"
-          onChange={handleChange}
-          className="select-part-report"
-          options={[
-            {
-              value: "jack",
-              label: "Jack",
-            },
-            {
-              value: "lucy",
-              label: "Lucy",
-            },
-            {
-              value: "Yiminghe",
-              label: "yiminghe",
-            },
-          ]}
-        />
+        <div className="range-picker-parent">
+          <label className="label-part-date-filter">
+            Start Date To End Date
+          </label>
+          <DatePicker.RangePicker
+            cellRender={cellRender}
+            className="rangePicker"
+            onChange={(dates) =>
+              dispatch({ type: "SET_DATE_RANGE", payload: dates })
+            }
+          />
+        </div>
+        <div className="select-part-parent-filter">
+          <label className="label-part-date-filter">
+            Start Date To End Date
+          </label>
+
+          <Select
+            defaultValue={state.select1}
+            onChange={(value) =>
+              dispatch({ type: "SET_SELECT1", payload: value })
+            }
+            className="select-part-report"
+            options={[
+              { value: "jack", label: "Jack" },
+              { value: "lucy", label: "Lucy" },
+              { value: "Yiminghe", label: "Yiminghe" },
+            ]}
+          />
+        </div>
+        <div className="select-part-parent-filter">
+          <label className="label-part-date-filter">
+            Start Date To End Date
+          </label>
+
+          <Select
+            defaultValue={state.select2}
+            onChange={(value) =>
+              dispatch({ type: "SET_SELECT2", payload: value })
+            }
+            className="select-part-report"
+            options={[
+              { value: "jack", label: "Jack" },
+              { value: "lucy", label: "Lucy" },
+              { value: "Yiminghe", label: "Yiminghe" },
+            ]}
+          />
+        </div>
         <div className="search-input-field">
+          <label className="label-part-date-filter"> Search here</label>
           <input
             type="text"
             placeholder="Search here"
             className="search-input-tag"
+            value={state.searchText}
+            onChange={(e) =>
+              dispatch({ type: "SET_SEARCH_TEXT", payload: e.target.value })
+            }
           />
         </div>
-
         <div className="button-submit-date-filter">
-          <button className="submit-btn-filter">Go</button>
-          <button className="submit-btn-filter">Reset</button>
+          <button className="submit-btn-filter" onClick={handleSubmit}>
+            Go
+          </button>
+          <button
+            className="submit-btn-filter"
+            onClick={() => dispatch({ type: "RESET" })}
+          >
+            Reset
+          </button>
         </div>
       </div>
     </div>
