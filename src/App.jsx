1b36491cc12318b979e7da5f92c939307540ea5e
DSRCODE <daljeetkalyanwat62@gmail.com>
2025-02-12 12:08:46 +0530

    api setup added, and protected route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,42 @@
-import './App.css'
-import DateFilter from './Components/DateFilter/DateFilter'
-import ControlPage from './Components/EventControl/ControlPage'
-import Header from './Components/Header/Header'
-import Login from './Components/Login/Login'
-import TopDashboard from './Components/Topdashboard/TopDashboard'
-import Layout from './Layout/Layout'
-import ClientAccountForm from './Pages/ClientAccountForm'
-import Clients from './Pages/Clients'
-import Dashboard from './Pages/Dashboard'
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Layout from "./Layout/Layout";
+import Dashboard from "./Pages/Dashboard";
+import Clients from "./Pages/Clients";
+import ClientAccountForm from "./Pages/ClientAccountForm";
+import ControlPage from "./Components/EventControl/ControlPage";
+import Login from "./Components/Login/Login";
+import { AuthProvider } from "./Context/AuthProvider";
+import ProtectedRoute from "./ProtectedRoute/ProtectedRoute";
 
 function App() {
   return (
-    <>
-      <Layout />
-      <Router>
+    <Router>
+      <AuthProvider>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/clients" element={<Clients />} />
-          <Route path="edit-user/:user" element={<ClientAccountForm />} />
-          <Route
-            path="gamebetlock/:user"
-            element={<ControlPage />}
-          />
-        </Routes>
-      </Router>
+          {/* Public Route */}
+          <Route path="/login" element={<Login />} />
 
-      {/* <Header/>
-    <TopDashboard/>
-    <Dashboard/>
-    <ControlPage/> */}
-      {/* <Login/> */}
-    </>
+          {/* Protected Routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/clients" element={<Clients />} />
+              <Route path="/edit-user/:user" element={<ClientAccountForm />} />
+              <Route path="/gamebetlock/:user" element={<ControlPage />} />
+            </Route>
+          </Route>
+
+          {/* Redirect unknown routes */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
   );
 }
 
---
+++ src/Utils/EncryptData.js
@@ -0,0 +1,17 @@
+import CryptoJS from "crypto-js";
+
+const SecretKey = import.meta.env.VITE_SECRET_KEY;
+
+export const encryptData = (data, secretKey = SecretKey) => {
+  if (!secretKey) {
+    console.error("Encryption failed: Secret key is missing.");
+    return null;
+  }
+  const key = CryptoJS.enc.Hex.parse(secretKey);
+  const dataToEncrypt = typeof data === "string" ? data : JSON.stringify(data);
+  const encrypted = CryptoJS.AES.encrypt(dataToEncrypt, key, {
+    mode: CryptoJS.mode.ECB,
+    padding: CryptoJS.pad.Pkcs7,
+  }).toString();
+  return { data: encrypted };
+};
